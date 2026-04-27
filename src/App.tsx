d95614a3b3690ab60dfc60133d77c5863b810730
Logan Klein <logan@e-rigging.com>
2026-04-27 12:19:35 -0500

    Remove technical docs from homepage

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { Process } from "@/components/sections/Process";
 import { Approvals } from "@/components/sections/Approvals";
 import { About } from "@/components/sections/About";
 import { WhatIsTdg } from "@/components/sections/WhatIsTdg";
-import { TechnicalInfo } from "@/components/sections/TechnicalInfo";
 import { Faq } from "@/components/sections/Faq";
 import { Contact } from "@/components/sections/Contact";
 
@@ -75,9 +74,6 @@ function App() {
 
         {/* Application Process */}
         <Process />
-
-        {/* Technical Info */}
-        <TechnicalInfo />
 
         {/* FAQ section */}
         <Faq />
